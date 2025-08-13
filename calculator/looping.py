19ddf7308e592041217c6eff3886e3993841f041
Brian Mwangi <brianmwanginjeri20@gmail.com>
2025-08-12 23:23:55 -0700

    initial commit

---
+++ calculator/looping.py
@@ -0,0 +1,12 @@
+july_cohort ={"apple", "banana", "cherry"}
+
+for x in july_cohort:
+
+    print(f"I love {x}!")
+
+    #print("i love banana!")
+    #pprint("i love cherry!"
+    #print("I love apple!")
+
+    for number in range(1, 4):
+        print(f"{x} is number {number} in the list.")
