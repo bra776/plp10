742a3eaea0f0ee28a51319e68d04db9fb6f0cc3f
riqman <brianmwanginjeri20@gmail.com>
2025-08-26 00:25:36 +0300

    Add files via upload

--- answers.sql
+++ answers.sql
@@ -1,22 +1,18 @@
---- This SQL query retrieves the check number, payment date, and amount from the payments table.
-SELECT checkSELECT checkNumber, paymentDate, amount
-FROM payments;
---  This SQL query retrieves the order date, required date, and status from the orders table for orders that are currently 'In Process', ordered by order date in descending order.
-SELECT orderDate, requiredDate, status
-FROM orders
-WHERE status = 'In Process'
-ORDER BY orderDate DESC;
---- This SQL query retrieves the first name, last name, and email of employees whose job title is 'Sales Rep', ordered by employee number in descending order.
-SELECT firstName, lastName, email
-FROM employees
-WHERE jobTitle = 'Sales Rep'
-ORDER BY employeeNumber DESC;
---- This SQL query retrieves all columns from the offices table.
-SELECT *
-FROM offices;
-
---- This SQL query retrieves the product name and quantity in stock from the products table, ordered by buy price in ascending order, limited to the first 5 results.
-SELECT productName, quantityInStock
-FROM products
-ORDER BY buyPrice ASC
+SELECT payment_date, SUM(amount) AS total_amount
+FROM payments
+GROUP BY payment_date
+ORDER BY payment_date DESC
 LIMIT 5;
+
+SELECT customer_name, country, AVG(credit_limit) AS average_credit_limit
+FROM customers
+GROUP BY customer_name, country;
+
+SELECT product_code, quantity_ordered, SUM(price_each * quantity_ordered) AS total_price
+FROM orderdetails
+GROUP BY product_code, quantity_ordered;
+
+SELECT check_number, MAX(amount) AS highest_amount
+FROM payments
+GROUP BY check_number;
+
